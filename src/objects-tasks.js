cf834e5732d4c6ab15c65a7b5645764f56cc5571
Roman <mamontov.r1995@gmail.com>
2024-03-11 22:19:22 +0300

    feat: add compareObj fn

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -77,8 +77,10 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  return Object.keys(obj1)
+    .map((key) => obj1[key] === obj2[key])
+    .every((compares) => compares);
 }
 
 /**
