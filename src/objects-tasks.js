6ad60ae0b413d2aa2031817c36f3a31075fe475e
Roman <mamontov.r1995@gmail.com>
2024-03-11 22:00:04 +0300

    feat: add removeProperties fn

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -59,8 +59,10 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const res = obj;
+  keys.forEach((key) => delete res[key]);
+  return res;
 }
 
 /**
