f7d9609763234ff2a15ddc061616f48d282e04fe
Roman <mamontov.r1995@gmail.com>
2024-03-14 00:00:29 +0300

    feat: add sellTickets fn

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -153,8 +153,16 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let change = 0;
+  queue.forEach((ticket) => {
+    if (ticket > 25) {
+      change -= ticket;
+    } else {
+      change += ticket;
+    }
+  });
+  return change >= 0;
 }
 
 /**
